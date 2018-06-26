6e2af36eb9bb2fb375e7b78fdcf669d11069ee16
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 02:42:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each {|name| badges.push(badge_maker(name))}
+  return badges
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index {|speaker,room| room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{room+1}!")}
+  return room_assignments
+end
+
+def printer(names)
+  batch_badge_creator(names).each {|badge| puts(badge)}
+  assign_rooms(names).each {|room_assign| puts(room_assign)}
+end
