8c1f963bef915645157f895897e2f7450c1e4a16
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 02:37:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creater(names)
+def batch_badge_creator(names)
   badges = []
   names.each {|name| badges.push(badge_maker(name))}
   return badges
